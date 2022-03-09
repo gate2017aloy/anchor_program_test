53f91b4076ae18cc7dcbaa6c471cf34b892e1339
Aloy Das <gate.2017.aloy@gmail.com>
2022-03-09 18:02:30 +0530

    dummy test case

--- programs/test-dapps/tests/lib.rs
+++ programs/test-dapps/tests/lib.rs
@@ -1,11 +1,18 @@
 use std::rc::Rc;
-use anchor_client::{Client, Cluster};
-use anchor_client::solana_sdk::signer::Signer;
+use anchor_lang::solana_program::system_program;
+use solana_sdk::signature::Keypair;
+use solana_sdk::{signature::Signer, transaction::Transaction};
 use solana_program_test::{processor, ProgramTest};
-use test_dapps::id;
 use test_dapps::{
-    accounts::Initialize,
+    accounts::Create as CreateAccounts,
+    instruction::Create as CreateInstruction,
+    id
 };
+use anchor_lang::{
+    InstructionData,
+    ToAccountMetas
+};
+use solana_sdk::instruction::Instruction;
 
 
 #[tokio::test]
@@ -17,16 +24,29 @@ async fn test() {
     );
     println!("Worked!!!");
 
-    let (mut banks_client, payer, recent_blockhash) = program_test.start().await;
-    let s: Rc<dyn Signer> = Rc::new(payer);
+    let base_account = Keypair::new();
 
-    let my_client = Client::new(Cluster::Localnet, s);
 
-    // let instructions = my_client
-    //     .program(id())
-    //     .request()
-    //     .accounts(test_dapps::accounts::Initialize{});
+    let mut program_context = program_test.start_with_context().await;
 
-    // println!("{:?}", test_dapps::accounts::Initialize{})
+    let create_ix = CreateInstruction{};
 
+    let ix = Instruction::new_with_bytes(
+        id(),
+        create_ix.data().as_ref(),
+        CreateAccounts {
+            base_account: base_account.pubkey(),
+            user: program_context.payer.pubkey(),
+            system_program: system_program::id()
+        }.to_account_metas(None)
+    );
+
+    let mut tx = Transaction::new_with_payer(&[ix], Some(&program_context.payer.pubkey()));
+    tx.sign(
+        &[&base_account, &program_context.payer],
+        program_context.last_blockhash,
+    );
+    let res = program_context.banks_client.process_transaction(tx).await;
+    println!("{:#?}", res);
+    println!("Worked!!!");
 }
